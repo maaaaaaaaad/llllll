fbf3c1a6a904a9c294467865a49bb650203e9d38
maaaaaaaaad <integral5137@gmail.com>
2022-12-10 19:47:04 +0900

    Initial Commit

---
+++ Spotify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spotify
+//
+//  Created by integral on 2022/12/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
