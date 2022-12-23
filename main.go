688e358425844f5890b314e96b6e815beceb499a
MAD <74044653+maaaaaaaaad@users.noreply.github.com>
2022-12-23 06:29:54 +0000

    qwe

--- main.go
+++ main.go
@@ -2,14 +2,11 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"time"
 )
 
 func main() {
-	rand.Seed(time.Now().UnixNano())
-	var key = []string{"mad", "woong", "integral"}
-	var index = rand.Intn(3)
-	result := key[index]
-	fmt.Println(result)
+	name := []string{"mad", "woong", "integral"}
+	for index, name := range name {
+		fmt.Println(index, name)
+	}
 }
