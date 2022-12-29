64c090c59dc78ca9d8a2c7d2d842d35cc22504d5
MAD <74044653+maaaaaaaaad@users.noreply.github.com>
2022-12-29 09:55:46 +0000

    e

--- main.go
+++ main.go
@@ -5,9 +5,16 @@ import (
 	"main/lib"
 )
 
-func main() {
-	num, changeNum := 2, 10
-	fmt.Println("init num:", num)
-	lib.ChangeNumber(&num, changeNum)
-	fmt.Println("change from pointer num:", num)
+func chance(a int, b int, f func(int, int) (r1 int, r2 int)) (x int, y int) {
+	x, y := f(a,b)
 }
+
+func change(a *int, b *int) (r1 int, r2 int) (r1 int, r2 int) {
+  r1 = *a * 100
+  r2 = *b * 100
+  return r1, r2
+}
+
+func main() {
+	p, q := 10
+  }
