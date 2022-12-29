daa8193bec18041ffcc2cc088346a628176b948d
MAD <74044653+maaaaaaaaad@users.noreply.github.com>
2022-12-29 10:25:42 +0000

    sef

--- main.go
+++ main.go
@@ -1,21 +1,17 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
 )
 
-func chance(a *int, b *int, f func(*int, *int) (r1 int, r2 int)) (x int, y int) {
-	x, y := f(a,b)
-}
-
-func change(a *int, b *int) (r1 int, r2 int) (r1 int, r2 int) {
-  r1 = *a * 100
-  r2 = *b * 100
-  return r1, r2
+func rpointer(a *int, b *int) (r1 int, r2 int) {
+	r1 = *a * 100
+	r2 = *b * 100
+	return r1, r2
 }
 
 func main() {
 	p, q := 5, 6
-  result1, result2 := chance(p,q, change)
-  fmt.Println(p, q)  
+	result1, result2 := rpointer(&p, &q)
+	fmt.Println(result1, result2)
 }
