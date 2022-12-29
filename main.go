96091a350f38146ea17bfb9cf80f7e3c90112c81
MAD <74044653+maaaaaaaaad@users.noreply.github.com>
2022-12-29 12:33:03 +0000

    Ee

--- main.go
+++ main.go
@@ -4,14 +4,18 @@ import (
 	"fmt"
 )
 
-func rpointer(a *int, b *int) (r1 int, r2 int) {
-	r1 = *a * 100
-	r2 = *b * 100
-	return r1, r2
+func rpointer(a *int, b *int, f func(*int, *int) (int, int)) {
+	x, y := f(a, b)
+  fmt.Println(x, y)
+}
+
+func lounge(a *int, b *int) (r1 int, r2 int) {
+  r1 = *a * 100
+  r2 = *b * 100
+  return r1, r2
 }
 
 func main() {
 	p, q := 5, 6
-	result1, result2 := rpointer(&p, &q)
-	fmt.Println(result1, result2)
+  rpointer(&p, &q, lounge)
 }
