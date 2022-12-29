14ea84cf51cd0445856fbc0000ed2c14eea35ecc
MAD <74044653+maaaaaaaaad@users.noreply.github.com>
2022-12-29 15:44:58 +0000

    e

--- main.go
+++ main.go
@@ -4,12 +4,21 @@ import (
 	"fmt"
 )
 
-func change[T int | int8 | int16 | int32 | int64](a *T, b *T) (r1 T, r2 T) {
-	return *a * 10, *b * 5
-}
-
 func main() {
-	p, q := 5, 6
-	result1, result2 := change(&p, &q)
-	fmt.Println(result1, result2)
+	m := map[string]int{
+		"one": 1,
+		"two": 2,
+	}
+	n := m
+	n["one"] = 5
+
+	p := make([]int, 5, 5)
+	q := p
+	q[0] = 10
+	fmt.Println(p, q)
+
+	x := [3]int{1, 2, 3}
+	y := x
+	y[0] = 10
+	fmt.Println(x, y)
 }
