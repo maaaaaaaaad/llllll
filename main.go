2e83586581bb9990cfe1189230c0e0a746e6fab5
MAD <74044653+maaaaaaaaad@users.noreply.github.com>
2022-12-30 06:18:39 +0000

    qwe

--- main.go
+++ main.go
@@ -5,20 +5,8 @@ import (
 )
 
 func main() {
-	m := map[string]int{
-		"one": 1,
-		"two": 2,
-	}
-	n := m
-	n["one"] = 5
-
-	p := make([]int, 5, 5)
-	q := p
-	q[0] = 10
-	fmt.Println(p, q)
-
 	x := [3]int{1, 2, 3}
-	y := x
+	y := x[:]
 	y[0] = 10
 	fmt.Println(x, y)
 }
