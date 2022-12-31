8a2efdb43bad8773cdfcf13a8555dac44ca875e0
MAD <74044653+maaaaaaaaad@users.noreply.github.com>
2022-12-31 12:11:26 +0000

    q

--- main.go
+++ main.go
@@ -4,9 +4,14 @@ import (
 	"fmt"
 )
 
+type person struct {
+	name string
+	age  int
+}
+
 func main() {
-	x := [3]int{1, 2, 3}
-	y := x[:]
-	y[0] = 10
-	fmt.Println(x, y)
+	mad := person{}
+	mad.name = "mad"
+	mad.age = 31
+	fmt.Println(mad)
 }
