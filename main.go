2e0fb29910619a44c607749d423fca6a500c9ec0
MAD <74044653+maaaaaaaaad@users.noreply.github.com>
2023-01-05 12:24:16 +0000

    qwe

--- main.go
+++ main.go
@@ -4,20 +4,6 @@ import (
 	"fmt"
 )
 
-type person struct {
-	name string
-	age  int
-}
-
-func (person) greeting() string {
-	return "hello world!"
-}
-
 func main() {
-	mad := person{
-		name: "mad",
-		age:  31,
-	}
-	welcome := mad.greeting()
-	fmt.Println(welcome)
+	fmt.Println("e")
 }
