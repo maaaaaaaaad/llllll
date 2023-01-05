78d144471c11478defd5a946ffead224a9ebb2cd
MAD <74044653+maaaaaaaaad@users.noreply.github.com>
2023-01-05 13:42:48 +0000

    Q

--- main.go
+++ main.go
@@ -4,6 +4,24 @@ import (
 	"fmt"
 )
 
+type person struct {
+  name string
+  age int
+}
+
+func (receiver *person) changer(name string , age int) {
+  receiver.name =  name
+  receiver.age = age
+}
+
 func main() {
-	fmt.Println("e")
+ var human *person = new(person)
+  human.name = "mad"
+  human.age = 22
+
+  fmt.Println(human)
+
+  human.changer("in", 20)
+
+  fmt.Println(human)
 }
