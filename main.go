9c18284ea0d614ee3f2f103057b0863ba795710a
MAD <74044653+maaaaaaaaad@users.noreply.github.com>
2023-01-12 07:24:07 +0000

    qwe

--- main.go
+++ main.go
@@ -14,15 +14,25 @@ type functions interface {
 	bar() int
 }
 
-func foo() int {}
-func bar() int {}
+func (receiver mock) foo() int {
+	return receiver.first
+}
+func (receiver mock) bar() int {
+	return receiver.second
+}
 
 func call(arg functions) (int, int) {
-	a := foo()
-	b := bar()
+	a := arg.foo()
+	b := arg.bar()
 	return a, b
 }
 
 func main() {
-  
+	n := mock{
+		first:  1,
+		second: 2,
+	}
+
+	r1, r2 := call(n)
+	fmt.Println(r1, r2)
 }
