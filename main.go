49fa9c4cd96332be15ebbf40b97c8e19b99d6ef1
MAD <74044653+maaaaaaaaad@users.noreply.github.com>
2023-01-14 06:03:31 +0000

    wef

--- main.go
+++ main.go
@@ -2,37 +2,32 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
-type mock struct {
-	first  int
-	second int
-}
-
-type functions interface {
-	foo() int
-	bar() int
-}
-
-func (receiver mock) foo() int {
-	return receiver.first
-}
-func (receiver mock) bar() int {
-	return receiver.second
-}
-
-func call(arg functions) (int, int) {
-	a := arg.foo()
-	b := arg.bar()
-	return a, b
+type person struct {
+	name string
 }
 
 func main() {
-	n := mock{
-		first:  1,
-		second: 2,
+
+	m := []person{
+		{
+			name: "mad",
+		},
+		{
+			name: "intel",
+		},
+		{
+			name: "gogo",
+		},
+		{
+			name: "gral",
+		},
 	}
 
-	r1, r2 := call(n)
-	fmt.Println(r1, r2)
+	rand.Seed(time.Now().UnixNano())
+	random := rand.Intn(3 + 1)
+	fmt.Println(m[random])
 }
