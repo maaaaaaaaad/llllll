e5d98136d699d9237c3071583b8a3010121f7e3d
MAD <74044653+maaaaaaaaad@users.noreply.github.com>
2023-01-18 10:45:48 +0000

    F

--- main.go
+++ main.go
@@ -4,8 +4,16 @@ import (
 	"fmt"
 )
 
+func foo(n uint8) uint8{
+  fmt.Println(&n)
+  return n
+}
+
 func main() {
-	var memory int
-	memory = 4
-	fmt.Printf("memory address %p, value %d", &memory, memory)
+	var memory uint8
+	memory = 7
+  fmt.Println(&memory)
+  result := foo(memory)
+  fmt.Println(&result)
+  fmt.Println(&result == &memory)
 }
