fa913ebf84199a3dc91f9e01684251f9c663a9ac
MAD <74044653+maaaaaaaaad@users.noreply.github.com>
2023-01-29 04:46:18 +0000

    qwe

--- main.go
+++ main.go
@@ -1,26 +1,3 @@
 package main
 
-import "fmt"
-
-func changeA(args [5]uint8) {
-	args[0] = 100
-}
-func changeS(args []uint8) {
-	args[0] = 100
-}
-
-func main() {
-	a := [5]uint8{1, 2, 3, 4, 5}
-	s := []uint8{1, 2, 3, 4, 5}
-
-	changeA(a)
-	changeS(s)
-
-	p := []uint8{1, 2, 3}
-
-	q := append(p, 4, 5)
-	fmt.Println("q", q)
-
-  p = append(p, 100)
-  fmt.Println("p", p)
-}
+func main() {}
