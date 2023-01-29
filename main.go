d98943d8bdf0c6e1d627f3b540a22beedb88199e
MAD <74044653+maaaaaaaaad@users.noreply.github.com>
2023-01-29 11:17:01 +0000

    e

--- main.go
+++ main.go
@@ -3,14 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	a := [5]uint8{1, 2, 3, 4, 5}
-	s := a[1:2]
-	fmt.Println(s)
+	a := [5]uint8{1, 2, 3, 4, 5} // len 5, cap 5
+	s := a[1:2]                  // len 1, cap 4
 
-	s[0] = 100
-	fmt.Println("a", len(a), cap(a), a)
-	fmt.Println("s:", len(s), cap(s), s)
+	s[0] = 100 // s = [100], len 1, cap 4
 
-	s = append(s, 200, 250, 255)
-	fmt.Println("s:", len(s), cap(s), s)
+	s = append(s, 200, 250, 255) // s = [100, 200, 250, 255], len 4, cap 4
+	s = append(s, 180)           // s = [100, 200, 250, 255, 180], len 5, cap 8
+	fmt.Printf("s: %v, len: %d, cap: %d\n", s, len(s), cap(s))
+	fmt.Println("a:", a)
 }
