12450a719deaf9b8adae7b4649d7110d6acda535
MAD <74044653+maaaaaaaaad@users.noreply.github.com>
2023-01-29 04:56:12 +0000

    e

--- main.go
+++ main.go
@@ -1,3 +1,21 @@
 package main
 
-func main() {}
+import "fmt"
+
+func main() {
+	s1 := []uint8{1, 2, 3}
+	s2 := append(s1, 4, 5)
+
+	fmt.Println("s1:", s1, len(s1), cap(s1))
+	fmt.Println("s2:", s2, len(s2), cap(s2))
+
+	s1[0] = 255
+
+	fmt.Println("s1:", s1, len(s1), cap(s1))
+	fmt.Println("s2:", s2, len(s2), cap(s2))
+
+	s1 = append(s1, 255)
+
+	fmt.Println("s1:", s1, len(s1), cap(s1))
+	fmt.Println("s2:", s2, len(s2), cap(s2))
+}
