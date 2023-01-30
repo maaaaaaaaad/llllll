15229c465e65d5ba83503ab23b77fcdf8a0b9aae
MAD <74044653+maaaaaaaaad@users.noreply.github.com>
2023-01-30 12:24:21 +0000

    e

--- main.go
+++ main.go
@@ -4,16 +4,9 @@ import "fmt"
 
 func main() {
 	s1 := []uint8{1, 2, 3, 4, 5}
-	s2 := make([]uint8, len(s1))
+	p := make([]uint8, len(s1), cap(s1))
 
-	for i, v := range s1 {
-		s2[i] = v
-	}
-
-	fmt.Printf("s2: %v, len: %d, cap: %d\n", s2, len(s2), cap(s2)) // {1,2,3,4,5}, 5, 5
-
-	s2[0] = 255
-
-	fmt.Println("s1:", s1)
-	fmt.Println("s2:", s2)
+	s2 := copy(p, s1)
+	fmt.Println(s2)
+	fmt.Println("p:", p)
 }
