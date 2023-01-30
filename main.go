fe024fa1dd6a17279a787768315dc0f347c03992
MAD <74044653+maaaaaaaaad@users.noreply.github.com>
2023-01-30 14:23:02 +0000

    e

--- main.go
+++ main.go
@@ -9,4 +9,15 @@ func main() {
 	s2 := copy(p, s1)
 	fmt.Println(s2)
 	fmt.Println("p:", p)
+
+	s3 := make([]uint8, 5, 5)
+	for i := 0; i < len(s3); i++ {
+		s3[i] = uint8(i + 1)
+	}
+	fmt.Println("s3:", s3)
+
+	s4 := make([]uint8, len(s3), cap(s3))
+	copy(s4, s3)
+	s4[0] = 255
+	fmt.Println("s4:", s4)
 }
