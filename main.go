69739f7fc690ccf48fbabd4003daed8706cb9d18
MAD <74044653+maaaaaaaaad@users.noreply.github.com>
2023-02-04 13:07:22 +0000

    e

--- main.go
+++ main.go
@@ -1,18 +1,35 @@
 package main
 
-import ("fmt")
+import (
+	"fmt"
+)
 
-type gogo interface{
-  one(n int) int
+type checkfields struct {
+	m int
+	n string
+	l float64
 }
 
-func one(n int) int {
-  return n + 100
+func check(v interface{}) {
+	switch t := v.(type) {
+	case int:
+		fmt.Printf("t is %d\n", t)
+	case string:
+		fmt.Printf("t is %s\n", t)
+	case float64:
+		fmt.Printf("t is %f\n", t)
+	default:
+		fmt.Println("default:", t)
+	}
 }
 
 func main() {
-  m := 1
-  var n gogo
-  r := n.one(m)
-  fmt.Println(r)
+	var a checkfields = checkfields{
+		m: 5,
+		n: "hello world",
+		l: 4.3,
+	}
+	check(a.m)
+	check(a.n)
+	check(a.l)
 }
