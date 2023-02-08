4d2b96721af1ac7569ac36d9575f5178ed201a7e
MAD <74044653+maaaaaaaaad@users.noreply.github.com>
2023-02-08 08:18:33 +0000

    e

--- main.go
+++ main.go
@@ -1,26 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
-type bundle interface {
-	checkInt(n interface{}) bool
-}
-
-func checkInt(n interface{}) bool {
-	switch n.(type) {
-	case int:
-		return true
-	default:
-		return false
-	}
-}
-
 func main() {
-	n, m := 255, "hi"
-	r1 := checkInt(n)
-	r2 := checkInt(m)
-  fmt.Print("r1:", r1)
-  fmt.Print("r1:", r1)
+
 }
