216e420988aaaa43a089a73a9f2f74b14d7e7ae4
MAD <74044653+maaaaaaaaad@users.noreply.github.com>
2023-02-08 08:26:32 +0000

    e

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+type per struct {
+	name string
+	age  uint8
+}
+
+type met interface {
+	upgrade(n uint8)
+}
+
+func (receiver *per) met(n uint8) {
+	receiver.age += n
+}
+
+func main() {
+	m := per{
+		name: "mad",
+		age:  20,
+	}
+
+	fmt.Println(m)
+
+	m.met(2)
+	fmt.Println(m)
 }
