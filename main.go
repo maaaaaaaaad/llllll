f573bab0991f743801ac3ba1650000d484a22248
MAD <74044653+maaaaaaaaad@users.noreply.github.com>
2023-02-10 08:14:15 +0000

    e

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import "fmt"
 
+type peer struct {
+	id          uint8
+	primary     string
+	information string
+	location    string
+}
+
+type load interface {
+	reload(peer)
+}
+
+func (receiver *peer) reload(peer peer) {
+	receiver.id = peer.id
+	receiver.primary = peer.primary
+	receiver.information = peer.information
+	receiver.location = peer.location
+}
+
+func main() {
+	m := peer{
+		id:          1,
+		primary:     "abc",
+		information: "he",
+		location:    "x",
+	}
+	fmt.Println(m)
 }
