0b60dc906f1dfd758b9f82633cc59b137e64961d
MAD <74044653+maaaaaaaaad@users.noreply.github.com>
2023-02-15 09:35:19 +0000

    e

--- main.go
+++ main.go
@@ -4,6 +4,12 @@ import (
 	"fmt"
 )
 
+func checkSlice(args []int) {
+	for i := 0; i < len(args); i++ {
+		args[i] += i + 10
+	}
+}
+
 func main() {
 	var a = make([]int, 5, 5)
 	for i := 0; i < len(a); i++ {
@@ -18,5 +24,8 @@ func main() {
 	b[0] = 100
 
 	fmt.Println("after a", a)
+	fmt.Println("after b", b)
+
+	checkSlice(b)
 	fmt.Println("after b", b)
 }
