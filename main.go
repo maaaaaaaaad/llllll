a4261e154b7fd743a292af0fdc53dbc6eee5024b
MAD <74044653+maaaaaaaaad@users.noreply.github.com>
2023-02-16 09:09:51 +0000

    e

--- main.go
+++ main.go
@@ -2,34 +2,30 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
-type slice interface {
-	checkSlice(args []int)
-}
-
-func checkSlice(args []int) {
-	for i := 0; i < len(args); i++ {
-		args[i] += i + 10
-	}
+type member struct {
+	name string
 }
 
 func main() {
-	var a = make([]int, 5, 5)
-	for i := 0; i < len(a); i++ {
-		a[i] = i + 1
+	rand.Seed(time.Now().UnixNano())
+	var key uint8 = uint8(rand.Intn(3) + 1)
+	fmt.Println(key)
+
+	var members = []member{
+		member{
+			name: "mad",
+		},
+		member{
+			name: "integral",
+		},
+		member{
+			name: "mooze",
+		},
 	}
 
-	var b = make([]int, len(a), cap(a))
-	copy(b, a)
-	fmt.Println("a", a)
-	fmt.Println("b", b)
-
-	b[0] = 100
-
-	fmt.Println("after a", a)
-	fmt.Println("after b", b)
-
-	checkSlice(b)
-	fmt.Println("after b", b)
+	fmt.Println(members)
 }
