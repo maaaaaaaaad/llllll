983d900d78b4fb3f21ab43371dafe744479d96ca
MAD <74044653+maaaaaaaaad@users.noreply.github.com>
2023-02-18 11:29:03 +0000

    e

--- main.go
+++ main.go
@@ -7,17 +7,15 @@ import (
 
 func main() {
 	v := list.New()
-	el1 := v.PushBack(1)
-	el2 := v.PushFront(2)
-	v.InsertAfter(3, el1)
-	v.InsertBefore(5, el2)
-	fmt.Println(v)
-
-  for e := v.Front(); e != nil; e = e.Next() {
-    fmt.Println(e.Value, " ")
-  }
-
-  for e := v.Back(); e != nil; e = e.Next() {
-    fmt.Println(e.Value, " ")
-  }
+	f := v.PushFront(1)
+	b := v.PushBack(2)
+	for e := v.Front(); e != nil; e = e.Next() {
+		fmt.Println(e.Value)
+	}
+	v.InsertAfter(10, f)
+	v.InsertBefore(20, b)
+	fmt.Println()
+	for e := v.Front(); e != nil; e = e.Next() {
+		fmt.Println(e.Value) // 1,10,20,2
+	}
 }
