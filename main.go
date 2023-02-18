725c41d41448b53d9d5c2b079d463a2e44f06d89
MAD <74044653+maaaaaaaaad@users.noreply.github.com>
2023-02-18 11:21:24 +0000

    e

--- main.go
+++ main.go
@@ -1,20 +1,23 @@
 package main
 
 import (
+	"container/list"
 	"fmt"
-	"math/rand"
-	"time"
 )
 
 func main() {
-	rand.Seed(time.Now().UnixNano())
+	v := list.New()
+	el1 := v.PushBack(1)
+	el2 := v.PushFront(2)
+	v.InsertAfter(3, el1)
+	v.InsertBefore(5, el2)
+	fmt.Println(v)
 
-	members := []string{
-		"mad",
-		"integral",
-		"woong",
-	}
+  for e := v.Front(); e != nil; e = e.Next() {
+    fmt.Println(e.Value, " ")
+  }
 
-	random := uint8(rand.Intn(len(members)))
-	fmt.Println(members[random])
+  for e := v.Back(); e != nil; e = e.Next() {
+    fmt.Println(e.Value, " ")
+  }
 }
