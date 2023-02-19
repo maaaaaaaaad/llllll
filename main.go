ab82fb69cebf63582f9bf8db6eaf6cf1a1bb3721
MAD <74044653+maaaaaaaaad@users.noreply.github.com>
2023-02-19 09:07:44 +0000

    e

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+type Product struct {
+	Name  string
+	Price int
+}
+
+func main() {
+	m := make(map[int]Product)
+	m[0] = Product{Name: "a", Price: 100}
+	m[1] = Product{Name: "b", Price: 200}
+	m[2] = Product{Name: "c", Price: 300}
+
+	for i, v := range m{
+    fmt.Println(i, v)
+  }
 }
