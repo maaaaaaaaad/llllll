d62b26e18207491f1fa62d4eea0339ed3fd2a4ce
MAD <74044653+maaaaaaaaad@users.noreply.github.com>
2023-02-19 07:59:39 +0000

    e

--- main.go
+++ main.go
@@ -1,21 +1,5 @@
 package main
 
-import (
-	"container/list"
-	"fmt"
-)
-
 func main() {
-	v := list.New()
-	f := v.PushFront(1)
-	b := v.PushBack(2)
-	for e := v.Front(); e != nil; e = e.Next() {
-		fmt.Println(e.Value)
-	}
-	v.InsertAfter(10, f)
-	v.InsertBefore(20, b)
-	fmt.Println()
-	for e := v.Front(); e != nil; e = e.Next() {
-		fmt.Println(e.Value) // 1,10,20,2
-	}
+
 }
