8f55a93de879adedfb4b9cc1ecda160d21606880
MAD <74044653+maaaaaaaaad@users.noreply.github.com>
2023-02-24 10:33:39 +0000

    e

--- main.go
+++ main.go
@@ -1,46 +1,15 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
-
-type Shape interface {
-	area() float64
-	perimeter() float64
-}
-
-type Rect struct {
-	width, height float64
-}
-
-type Circle struct {
-	radius float64
-}
-
-func (r Rect) area() float64 { return r.width * r.height }
-
-func (r Rect) perimeter() float64 {
-	return 2 * (r.width + r.height)
-}
-
-func (c Circle) area() float64 {
-	return math.Pi * c.radius * c.radius
-}
-func (c Circle) perimeter() float64 {
-	return 2 * math.Pi * c.radius
-}
+import "fmt"
 
 func main() {
-	r := Rect{10., 20.}
-	c := Circle{10}
+	var x interface{}
+	x = 1
+	x = "Tom"
 
-	showArea(r, c)
+	printIt(x)
 }
 
-func showArea(shapes ...Shape) {
-	for _, s := range shapes {
-		a := s.area()
-		fmt.Println(a)
-	}
+func printIt(v interface{}) {
+	fmt.Println(v) //Tom
 }
