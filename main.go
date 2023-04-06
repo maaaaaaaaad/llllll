ccdb27fc22606dbf808c8ece14247bcca0e1a583
dongwoongoh <>
2023-04-06 08:32:36 +0000

    e

--- main.go
+++ main.go
@@ -2,14 +2,21 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"time"
+	"io/ioutil"
+	"net/http"
 )
 
 func main() {
-	rand.Seed(time.Now().UnixNano())
-	m := []int{1, 2, 3, 4}
-	for _, value := range m {
-		fmt.Println(value)
+	resp, err := http.Get("http://google.com")
+	if err != nil {
+		panic(err)
 	}
+
+	defer resp.Body.Close()
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s\n", string(data))
 }
