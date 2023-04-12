3742cc36bd7b6116541c972f3e681d843cf3b610
maaaaaaaaad <74044653+maaaaaaaaad@users.noreply.github.com>
2023-04-12 07:24:42 +0000

    e

--- main.go
+++ main.go
@@ -13,7 +13,18 @@ func sequence(n uint) uint {
 	}
 }
 
+func febSequnce(n uint) uint {
+	if n == 1 || n == 2 {
+		return 1
+	} else {
+		return sequence(n-1) + sequence(n-2)
+	}
+}
+
 func main() {
 	result := sequence(5)
 	fmt.Println(result)
+
+	feResult := febSequnce(5)
+	fmt.Println(feResult)
 }
