802198dde220db1c4e75980997de50e3398e1539
maaaaaaaaad <74044653+maaaaaaaaad@users.noreply.github.com>
2023-04-17 12:35:39 +0000

    e

--- main.go
+++ main.go
@@ -2,29 +2,11 @@ package main
 
 import "fmt"
 
-const head = 4
-const diff = 9
-
-func sequence(n uint) uint {
-	if n == 1 {
-		return head
-	} else {
-		return sequence(n-1) + diff
-	}
-}
-
-func febSequnce(n uint) uint {
-	if n == 1 || n == 2 {
-		return 1
-	} else {
-		return sequence(n-1) + sequence(n-2)
-	}
-}
-
 func main() {
-	result := sequence(5)
-	fmt.Println(result)
-
-	feResult := febSequnce(5)
-	fmt.Println(feResult)
+	people := []string{"a", "b", "c", "d", "e", "f"}
+	for i := 0; i < len(people); i++ {
+		for j := i + 1; j < len(people); j++ {
+			fmt.Printf("(%s, %s)\n", people[i], people[j])
+		}
+	}
 }
